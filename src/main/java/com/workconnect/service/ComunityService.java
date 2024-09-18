7823c645948372ca245413f73bfeb17dcaff4e47
Tomás Melo <67438238+TomasNM229@users.noreply.github.com>
2024-09-18 18:47:38 -0500

    Implemente comunity servce

--- src/main/java/com/workconnect/service/ComunityService.java
+++ src/main/java/com/workconnect/service/ComunityService.java
@@ -1,4 +1,12 @@
 package com.workconnect.service;
 
+import com.workconnect.dto.ComunityCreateUpdateDTO;
+import com.workconnect.dto.ComunityDetailsDTO;
+import org.springframework.stereotype.Service;
+
+
 public interface ComunityService {
+    ComunityDetailsDTO createComunity(ComunityCreateUpdateDTO comunityCreateUpdateDTO);
+
+
 }
